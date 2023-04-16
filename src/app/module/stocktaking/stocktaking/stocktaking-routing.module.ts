3d979074587c87d10f96b31a9590c6e7dda72f86
Jorge Cervantes <jcdev1825@gmail.com>
2023-04-15 22:15:38 -0600

    style(DX): :art: sort imports

--- src/app/module/stocktaking/stocktaking/stocktaking-routing.module.ts
+++ src/app/module/stocktaking/stocktaking/stocktaking-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { StocktakingComponent } from './stocktaking.component';
 import { RouterModule, Routes } from '@angular/router';
+import { StocktakingComponent } from './stocktaking.component';
 
 const routes: Routes = [{ path: '', component: StocktakingComponent }];
 
