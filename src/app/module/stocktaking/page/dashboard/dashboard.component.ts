0366208b16bb7024e33c1afdfc9cbbc1adf844ba
Jorge Cervantes <jcdev1825@gmail.com>
2023-04-26 00:15:07 -0600

    feat(User): :sparkles: add User service to shared data between components using BehaviorSubject

--- src/app/module/stocktaking/page/dashboard/dashboard.component.ts
+++ src/app/module/stocktaking/page/dashboard/dashboard.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { UserDto } from '@common-service/user/user.dto';
+import { UserService$ } from '@common-service/user/user.service';
 
 @Component({
 	selector: 'app-dashboard',
 	templateUrl: './dashboard.component.html',
 	styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+	// eslint-disable-next-line no-unused-vars
+	constructor(private userService$: UserService$) {
+		this.loadUser();
+	}
+
+	private loadUser(): void {
+		const user: UserDto = {
+			id: '1',
+			fullName: 'Jorge Cervantes',
+			username: 'devcoy',
+			email: 'jcdev1825@gmail.com',
+			lastSession: new Date().toISOString()
+		};
+		this.userService$.setCurrentUser(user);
+	}
+}
