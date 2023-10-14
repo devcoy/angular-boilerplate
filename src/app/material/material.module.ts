5638b19a985f69d647d928c1fceee79141acabc5
Jorge Cervantes <jcdev1825@gmail.com>
2023-10-13 20:12:23 -0600

    feat(material): set apparence as outline in form input

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,7 +2,10 @@ import { NgIf } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+	MAT_FORM_FIELD_DEFAULT_OPTIONS,
+	MatFormFieldModule
+} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
@@ -17,6 +20,12 @@ const material = [
 
 @NgModule({
 	exports: [material],
-	imports: [material]
+	imports: [material],
+	providers: [
+		{
+			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+			useValue: { appearance: 'outline' }
+		}
+	]
 })
 export class MaterialModule {}
